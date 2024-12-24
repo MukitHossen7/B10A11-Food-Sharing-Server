a6b61c5786f6f12cf8fa920e9429c38c3e667506
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-24 13:43:07 +0600

    token create and sequre in database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const { connection, client } = require("./DB/MongoDB");
 const { compareAsc, compareDesc } = require("date-fns");
 const { ObjectId } = require("mongodb");
@@ -9,24 +11,73 @@ const port = process.env.PORT || 5000;
 connection();
 
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+//Custom middleware
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const foodCollection = client.db("foodSharingBD").collection("foods");
 const requestedFoodCollection = client
   .db("foodSharingBD")
   .collection("requestFoods");
 
+//Create token use jwt
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
+
+app.post("/logout", async (req, res) => {
+  res
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
+
 //post all add foods
-app.post("/all-foods", async (req, res) => {
+app.post("/all-foods", verifyToken, async (req, res) => {
   const foods = req.body;
   const result = await foodCollection.insertOne(foods);
   res.send(result);
 });
 
 //post request foods
-app.post("/request-foods", async (req, res) => {
+app.post("/request-foods", verifyToken, async (req, res) => {
   const foodRequest = req.body;
+  if (req.user.email !== foodRequest.user_email) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
   const foodId = foodRequest.food_id;
   const result = await requestedFoodCollection.insertOne(foodRequest);
   const filter = { _id: new ObjectId(foodId) };
@@ -85,15 +136,17 @@ app.get("/featured-foods", async (req, res) => {
   res.send(foods);
 });
 //delete all food databases
-app.delete("/all-foods/:id", async (req, res) => {
+app.delete("/all-foods/:id", verifyToken, async (req, res) => {
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) };
   const result = await foodCollection.deleteOne(filter);
   res.send(result);
 });
-app.patch("/all-foods/:id", async (req, res) => {
+app.patch("/all-foods/:id", verifyToken, async (req, res) => {
   const food = req.body;
   const id = req.params.id;
+  console.log("updated", id);
+  console.log("verified email", req.user.email);
   const filter = { _id: new ObjectId(id) };
   const updateDoc = {
     $set: {
@@ -110,37 +163,35 @@ app.patch("/all-foods/:id", async (req, res) => {
 });
 
 // manage my foods by login email
-app.get("/manage-my-foods", async (req, res) => {
+app.get("/manage-my-foods", verifyToken, async (req, res) => {
   const email = req.query.email;
+  if (req.user.email !== email) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
   const query = { "donator.donatorEmail": email };
   const result = await foodCollection.find(query).sort({ _id: -1 }).toArray();
   res.send(result);
 });
 
 // get requested food by login user email
-app.get("/request-foods", async (req, res) => {
+app.get("/request-foods", verifyToken, async (req, res) => {
   const email = req.query.email;
+  if (req.user.email !== email) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
   const result = await requestedFoodCollection
     .find({ user_email: email })
     .toArray();
   res.send(result);
 });
 //get single food database use id
-app.get("/all-foods/:id", async (req, res) => {
+app.get("/all-foods/:id", verifyToken, async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
   const result = await foodCollection.findOne(query);
   res.send(result);
 });
-// app.get("/all-foods/:sortedFoods", async (req, res) => {
-//   const sortType = parseInt(req.params.sortedFoods);
-//   console.log(sortType);
-//   const foods = await foodCollection
-//     .find({ status: "Available" })
-//     .sort({ expireDate: sortType })
-//     .toArray();
-//   res.send(foods);
-// });
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Food sharing API!");
 });
